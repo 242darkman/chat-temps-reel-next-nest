8dd648d6723396cedde0dbd9d5c79b6374b480ba
242darkman <53907600+242darkman@users.noreply.github.com>
2023-12-04 23:36:28 +0100

    feat: ajout du layout de l'application ainsi que de la page d'accueil

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Messagerie chat intégrant du temps réel
  ## Technos utilisées :
- > * NextJS 14
- > * NestJS 9
- > * Socket IO
- > * Tailwindcss
+ > * NextJS v14.0.3
+ > * NestJS v9
+ > * Socket IO Client  v4.7.2
+ > * Tailwindcss v3.3
+ > * React icons v4.12.0
--- chat-real-time/src/app/page.js
+++ chat-real-time/src/app/page.js
@@ -1,8 +1,36 @@
+import BaseButton from '@/components/BaseButton.jsx';
+import { FaArrowRight } from 'react-icons/fa';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 export default function Home() {
+  const [username, setUsername] = useState('');
+  const router = useRouter();
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleChatRedirect = () => {
+    router.push('/chat');
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Piraterie 92I</h1>
-    </main>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl">Bienvenue sur ParleyPoint</h1>
+      <p className="mt-3 text-2xl">Entrez votre nom d'utilisateur pour commencer à discuter</p>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={handleUsernameChange}
+        className="mt-5 p-2 border rounded"
+      />
+      {username && (
+        <BaseButton onClick={handleChatRedirect} theme="primary">
+          Commencer la discussion <FaArrowRight />
+        </BaseButton>
+      )}
+    </div>
+  );
 }
