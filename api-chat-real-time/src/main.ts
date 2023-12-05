776500dd7ee47fd8bfc5ee207ce39f19abdca56b
242darkman <53907600+242darkman@users.noreply.github.com>
2023-12-05 19:24:30 +0100

    feat(core): on autorise le partage de ressource entre mon server et le domaine de mon client

--- api-chat-real-time/src/main.ts
+++ api-chat-real-time/src/main.ts
@@ -3,6 +3,16 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /**
+   * Permet le partage de ressources entre origines (CORS) avec une origine spécifique.
+   *
+   * @param {Object} options - L'objet de configuration pour CORS.
+   * @param {string[]} options.origin - Tableau des domaines d'origine autorisés.
+   */
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  });
   await app.listen(8001);
 }
 bootstrap();
