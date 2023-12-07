7ae41e04f5f6868b017cfc9ad5874af3465b9a9e
242darkman <53907600+242darkman@users.noreply.github.com>
2023-12-07 11:48:38 +0100

    feat: configuration du module `openai`

--- api-chat-real-time/src/app.module.ts
+++ api-chat-real-time/src/app.module.ts
@@ -1,10 +1,24 @@
 import { AppController } from './app.controller';
 import { ChatGateway } from './chat/chat.gateway';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { ChatService } from './chat/chat.service';
+import OpenAI from 'openai';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [ChatGateway],
+  providers: [
+    ChatGateway,
+    {
+      provide: OpenAI,
+      useValue: new OpenAI({ apiKey: process.env.OPENAI_API_KEY }),
+    },
+    ChatService,
+  ],
 })
 export class AppModule {}
