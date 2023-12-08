a5a6e1004cf1c754526cd797fb5c0d768269d13f
242darkman <53907600+242darkman@users.noreply.github.com>
2023-12-08 12:45:59 +0100

    feat: ajout d'une icone dans le bouton de traduction de la navbar et activation du split mode pour la selection de la langue de traduction

--- chat-real-time/src/app/(component)/BaseNavBar.jsx
+++ chat-real-time/src/app/(component)/BaseNavBar.jsx
@@ -1,7 +1,9 @@
 import BaseButton from './BaseButton.jsx';
+import { FaGlobeAfrica } from "react-icons/fa";
+import LANGUAGES from '../(utils)/app.constants.js';
 import React from 'react';
 
-const BaseNavBar = ({ onToggleSelectMode, selectedMessages }) => {
+const BaseNavBar = ({ onToggleSelectMode, selectedMessages, onTranslate, onVerify }) => {
   
   return (
     <div className="flex flex-col p-2 bg-gray-200 rounded-lg rounded-t-none">
@@ -17,8 +19,16 @@ const BaseNavBar = ({ onToggleSelectMode, selectedMessages }) => {
           selectedMessages && selectedMessages.length > 0 &&
           (
             <>
-              <BaseButton >Traduire</BaseButton>
-              <BaseButton theme='secondary'>Valider l'info</BaseButton>
+              <BaseButton
+                onClick={onTranslate}
+                splitMode={true}
+                icon={<FaGlobeAfrica />}
+                iconPosition='left'
+                options={LANGUAGES}
+              >
+                Traduire
+              </BaseButton>
+              <BaseButton theme='secondary' onClick={onVerify}>Valider l'info</BaseButton>
             </>
           )
         }
